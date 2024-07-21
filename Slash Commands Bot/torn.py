b9e71e2a6bc5843cad1d629f9e044261d3a259df
james-tierney <jamesytier@hotmail.com>
2024-07-21 16:56:33 +0100

    firebase again

--- Slash Commands Bot/torn.py
+++ Slash Commands Bot/torn.py
@@ -11,14 +11,24 @@ DISCORD_ID = os.getenv('DISCORD_ID')
 print("Torn token = ", TOKEN)
 print("DISCORD_ID = ", DISCORD_ID)
 
-# Path to your service account key
-SERVICE_ACCOUNT_KEY = os.getenv('SERVICE_ACCOUNT_KEY')
-print("SERVICE_ACCOUNT_KEY = ", SERVICE_ACCOUNT_KEY)
+# Create a dictionary with the environment variables
+firebase_credentials = {
+    "type": os.getenv("FIREBASE_TYPE"),
+    "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+    "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+    "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace('\\n', '\n'),
+    "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+    "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+    "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
+    "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
+    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL"),
+    "universe_domain": os.getenv("UNIVERSE_DOMAIN"),
+}
 
-# Initialize the Firebase Admin SDK
-cred = credentials.Certificate(SERVICE_ACCOUNT_KEY)
+# Initialize Firebase app
+cred = credentials.Certificate(firebase_credentials)
 firebase_admin.initialize_app(cred)
-
 
 def get_user_details():
     url = f'https://api.torn.com/user/?selections=profile&key={TOKEN}'
