27eceabe37829eecb37843e68d4ca8ca17508ce8
james-tierney <jamesytier@hotmail.com>
2024-07-21 16:40:12 +0100

    firebase migration

--- Slash Commands Bot/database.py
+++ Slash Commands Bot/database.py
@@ -8,13 +8,24 @@ load_dotenv()
 
 # Path to your service account key
 SERVICE_ACCOUNT_KEY = os.getenv('SERVICE_ACCOUNT_KEY')
+# Create a dictionary with the environment variables
+firebase_credentials = {
+    "type": os.getenv("FIREBASE_TYPE"),
+    "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+    "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+    "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace('\\n', '\n'),
+    "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+    "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+    "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
+    "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
+    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL"),
+    "universe_domain": os.getenv("UNIVERSE_DOMAIN"),
+}
 
-# Initialize the Firebase Admin SDK
-cred = credentials.Certificate(SERVICE_ACCOUNT_KEY)
+# Initialize Firebase app
+cred = credentials.Certificate(firebase_credentials)
 firebase_admin.initialize_app(cred)
-
-# Initialize Firestore DB
-db = firestore.client()
 
 def insert_user_key(discord_id, torn_id, torn_api_key):
     doc_ref = db.collection('user_keys').document(discord_id)
