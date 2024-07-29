107ae01c80166ece38d0d786f1f6e0f400bfc5e5
james-tierney <jamesytier@hotmail.com>
2024-07-29 20:01:17 +0100

    trying to debug timezone interaction failing on discord

--- Slash Commands Bot/timezone.py
+++ Slash Commands Bot/timezone.py
@@ -2,7 +2,6 @@ import discord
 from discord.ui import Select, View, Button
 from database import get_firestore_db
 
-# Define all UTC offsets
 UTC_OFFSETS = [f"TCT{n:+}" for n in range(-12, 15)]
 ITEMS_PER_PAGE = 25
 
@@ -30,37 +29,35 @@ class TimezoneView(View):
 
         # Add pagination buttons if needed
         if self.current_page > 0:
-            self.add_item(Button(label='Previous', style=discord.ButtonStyle.secondary))
+            self.add_item(Button(label='Previous', style=discord.ButtonStyle.secondary, custom_id='previous'))
         if (self.current_page + 1) * ITEMS_PER_PAGE < len(UTC_OFFSETS):
-            self.add_item(Button(label='Next', style=discord.ButtonStyle.secondary))
+            self.add_item(Button(label='Next', style=discord.ButtonStyle.secondary, custom_id='next'))
 
     async def select_callback(self, interaction):
         selected_tz = interaction.data['values'][0]
         discord_id = str(interaction.user.id)
-        db = get_firestore_db()  # Define this function as per your database setup
+        db = get_firestore_db()
 
         converted_to_utc_TZ = convert_tct_to_utc_offset(selected_tz)
-        # Store the selected UTC offset in Firestore
         db.collection('user_keys').document(discord_id).set({
-            'time_zone': convert_tct_to_utc_offset
+            'time_zone': converted_to_utc_TZ
         }, merge=True)
 
         await interaction.response.send_message(f"Time zone set to {selected_tz}", ephemeral=True)
 
-    @discord.ui.button(label="Previous", style=discord.ButtonStyle.secondary)
+    @discord.ui.button(label="Previous", style=discord.ButtonStyle.secondary, custom_id='previous')
     async def previous(self, button, interaction):
         if self.current_page > 0:
             self.current_page -= 1
             self.update_select_options()
             await interaction.response.edit_message(view=self)
 
-    @discord.ui.button(label="Next", style=discord.ButtonStyle.secondary)
+    @discord.ui.button(label="Next", style=discord.ButtonStyle.secondary, custom_id='next')
     async def next(self, button, interaction):
         if (self.current_page + 1) * ITEMS_PER_PAGE < len(UTC_OFFSETS):
             self.current_page += 1
             self.update_select_options()
             await interaction.response.edit_message(view=self)
-
 
 # Function to convert TCT offset to UTC offset
 def convert_tct_to_utc_offset(tct_str):
