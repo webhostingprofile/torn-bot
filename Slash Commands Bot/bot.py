6818e19f991f3c8ac7f29b92db1919e36ba83054
james-tierney <jamesytier@hotmail.com>
2024-08-01 20:10:19 +0100

    re ordered the info command text

--- Slash Commands Bot/bot.py
+++ Slash Commands Bot/bot.py
@@ -85,17 +85,20 @@ async def stats(ctx):
 async def info(ctx):
     info_message = (
         "**Bot Command Information**\n\n"
-        "**!timezone**\n"
-        "Use this command to set your timezone offset from Torn City Time (TCT). "
-        "When you call this command, the bot will prompt you to select your offset from TCT. "
-        "This helps in adjusting the time-related data based on your local timezone.\n\n"
-        
+
         "**!addkeys**\n"
         "This command is used to register your Torn ID and API key with the bot. "
         "Use this command in a direct message to the bot for security reasons. "
         "The syntax is `!addkeys <Torn_ID> <Torn_API_Key>`. "
         "The bot will store your Torn ID and API key securely, allowing it to access your "
         "Torn account information and provide personalized data.\n\n"
+
+        "**!timezone**\n"
+        "Use this command to set your timezone offset from Torn City Time (TCT). "
+        "When you call this command, the bot will prompt you to select your offset from TCT. "
+        "This helps in adjusting the time-related data based on your local timezone.\n\n"
+        
+
         
         "**Note:**\n"
         "- Make sure to send `!addkeys` in a private message (DM) to avoid exposing your API key publicly.\n"
