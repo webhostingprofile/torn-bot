c64aa0ba740b41cf92f7821cef2ddf87103afe95
james-tierney <jamesytier@hotmail.com>
2024-08-29 20:25:05 +0100

    debugging new users not getting stats removed profile link from stats command to test

--- Slash Commands Bot/torn.py
+++ Slash Commands Bot/torn.py
@@ -210,7 +210,7 @@ def get_user_stats(discord_id, discord_username):
                 )
             else:
                 user_details = (
-                    f"{profile_link}:\n\n"
+                    #f"{profile_link}:\n\n"
                     f"Old Battle Stats ---> New Battle Stats\n"
                     f"Str: {previous_stats.get('strength', 0):,}\n"
                     f"Spd: {previous_stats.get('speed', 0):,}\n"
